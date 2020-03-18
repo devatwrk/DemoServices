4e2da6693b807cf2f18242bd52aa126aff92ed57
avinash003 <55076731+avinash003@users.noreply.github.com>
2020-03-18 23:30:50 +0530

    Initial commit ViewBinding

---
+++ app/src/main/java/com/dev/demoservices/MainActivity.java
@@ -0,0 +1,48 @@
+package com.dev.demoservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.dev.demoservices.databinding.ActivityMainBinding;
+import com.dev.demoservices.utils.MyService;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+
+
+        binding.btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                startService(intent);
+            }
+        });
+
+        binding.btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                stopService(intent);
+            }
+        });
+
+    }
+}
